e4b2eda2a38eb18cd2d758539d90952f75b6d2df
Sonia Moretti <soni.moretti@gmail.com>
2024-06-30 12:32:26 -0600

    candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,16 +10,24 @@ election_csv = Path("PyPoll/Resources/election_data.csv")
 
 # Initialize Variables 
 total_votes = 0
+candidate_votes = {}
 
 
 #Read csv file
 with open(election_csv,'r') as file:
     reader= csv.DictReader(file)
     
-    # Iterate through each row in the dataset and count the votes
+    # Iterate through each row in the dataset
     for row in reader:
         total_votes += 1
+        candidate = row['Candidate']
+        
+        if candidate in candidate_votes:
+            candidate_votes[candidate] += 1
+        else:
+            candidate_votes[candidate]= 1
 
 # Print the total number of votes cast
 print(f"Total Votes: {total_votes}")
+print(candidate_votes)
     
